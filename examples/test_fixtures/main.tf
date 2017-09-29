6a912b33f708dfa7deaa1138e1f6eaf2bcfaab5c
brandoconnor <brandoconnor@gmail.com>
2017-09-28 17:22:06 -0700

    Comments added. Validating the working config in CI

--- examples/test_fixtures/main.tf
+++ examples/test_fixtures/main.tf
@@ -7,18 +7,21 @@ provider "aws" {
   version = "1.0.0"
 }
 
-# module "vpc" {
-#   source             = "terraform-aws-modules/vpc/aws"
-#   name               = "my-vpc"
-#   cidr               = "10.0.0.0/16"
-#   private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-#   enable_nat_gateway = "false"
-#   azs                = ["us-west-2a", "us-west-2b", "us-west-2c"]
-# }
+resource "null_resource" "debug_echo" {
+  triggers {
+    uuid = "${uuid()}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${path.module}"
+  }
+}
 
 module "main" {
+  # source = "/app/" if running with `docker run -i -t -v $(pwd):/app/ -w /app/examples/test_fixtures hashicorp/terraform:light init`
+
   source = "../../.."
+
   region = "${var.region}"
 
   tags {
